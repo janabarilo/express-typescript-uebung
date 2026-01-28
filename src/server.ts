30ebc61aaa02a4cbe96492b620b47f43f2caa2ec
Jana Koettel <janabarilo@protonmail.com>
2026-01-28 14:57:43 +0100

    Login-Endpunkt fertig

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,57 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import crypto from "crypto";
 import users from "../data/users.json";
 
-
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+const registeredUsers: Map<string, string> = new Map([
+  ["alice", "1234"],
+  ["bob", "secret"],
+  ["charlie", "qwerty"],
+]);
+
+const sessions: Map<string, string> = new Map(); // sessionId -> username
+
+app.post("/auth/login", (req, res) => {
+  console.log("BODY:", req.body);
+ 
+
+  const { username, password } = req.body;
+
+  // Check ob username und password existieren
+  if (!username || !password) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  // Existiert User?
+  const savedPassword = registeredUsers.get(username);
+  if (!savedPassword) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  // Passwort korrekt?
+  if (savedPassword !== password) {
+    return res.status(401).json({ error: "Invalid credentials" });
+  }
+
+  // Session erstellen
+  const sessionId = crypto.randomUUID();
+  sessions.set(sessionId, username);
+
+  // Cookie setzen (httpOnly!)
+  res.cookie("sessionId", sessionId, {
+    httpOnly: true,
+  });
+
+  return res.json({ message: "Login succeeded" });
+});
+
+
 
 app.get("/echo", (req, res) => {
   res.send("Echo");
